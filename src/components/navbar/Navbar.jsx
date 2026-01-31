1bfa4d45793004d13689b464c21e883b315ba27d
shohruhmirzo0507 <snomanja2000@gmail.com>
2026-01-31 13:41:44 +0500

    links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,22 @@
-import "./Navbar.css"
-import logos from "../../../public/logo.png"
+import "./Navbar.css";
+import logos from "../../../public/logo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
-    <nav>
-      <div className="nav__left">
-        <img className="logo__brand" src={logos} alt="" />
-         <h1>Target It school</h1>
-      </div>
-    </nav>
-
+      <nav>
+        <div className="nav__left">
+          <Link to="/">
+            <img className="logo__brand" src={logos} alt="" />
+          </Link>
+          <Link to="/">
+            <h1>Target It school</h1>
+          </Link>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
